657d8a67617dc4ad0f4e4d39d355fc6f504d6e69
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-19 01:35:24 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,6 +52,8 @@ class InteractiveRecord
   end
 
   def self.find_by_name
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
+    DB[:conn].execute(sql, name)
   end
 
   def self.find_by
